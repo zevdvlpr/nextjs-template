5006a9af308e65a5d7aa78c85f067360f9a7d050
zevdvlpr <zevdvlpr@gmail.com>
2020-07-27 22:49:46 -0300

    fix

--- src/api/Main.ts
+++ src/api/Main.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://localhost:3333';
 
 export default class MainAPI {
-  static getPosts() {
-    return axios.get(`${BASE_URL}/posts`);
+  static getPosts(num: number) {
+    return axios.get(`${BASE_URL}/posts?page=${num}`);
   }
 }
