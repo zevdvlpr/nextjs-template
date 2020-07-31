b22acf831c97e4723519c9ce419eb6203fa5ec81
zevdvlpr <zevdvlpr@gmail.com>
2020-07-31 12:30:45 -0300

    fix

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,7 +1,8 @@
-import Head from 'next/head';
 import React from 'react';
 
-interface Props {
+import Meta from './meta';
+
+export interface Props {
   title?: string;
   description?: string;
   image?: string;
@@ -16,42 +17,7 @@ const Layout: React.FC<Props> = ({
   children,
 }) => (
   <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
-      <title>{title}</title>
-
-      <link itemProp="url" href={url} />
-      <meta itemProp="name" content={title} />
-      <meta itemProp="description" content={description} />
-
-      <meta name="image" content={image} />
-      <meta name="description" content={description} />
-
-      <meta property="og:title" content={title} />
-      <meta property="og:url" content={url} />
-      <meta property="og:description" content={description} />
-
-      <meta property="og:image:width" content="1920" />
-      <meta property="og:image:height" content="1080" />
-
-      <meta property="og:image" content={image} />
-      <meta property="og:image:secure_url" content={image} />
-
-      <meta name="twitter:site" content="aa" />
-      <meta name="twitter:creator" content="aa" />
-      <meta name="twitter:card" content="summary_large_image" />
-
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-
-      <meta name="twitter:image" content={image} />
-      <meta name="twitter:image:src" content={image} />
-
-      <link rel="icon" href={image} />
-      <link rel="shortcut icon" href={image} />
-    </Head>
+    <Meta title={title} description={description} image={image} url={url} />
     {children}
   </>
 );
